e47b0add48605bbcd78f537beae4a51f0ba5f29a
shaheer0341 <muhammadshaheeredu@gmail.com>
2025-05-14 17:38:20 +0500

    delete

--- log in.cpp
+++ log in.cpp
@@ -1,7 +1,20 @@
+#define MAX 100
+#define FILENAME "records.dat"
+
+// Structure For Crime Records
+typedef struct {
+	int caseID ;
+	char criminalName[50] ;
+	char crimeType[50] ;
+	char crimeLocation[50] ;
+	char date[20] ;
+	char caseStatus[50] ;
+}CrimeRecord;
 
   int login(); //login function new
-  
   void addRecord();
+  void deleteRecord();    //delete function
+  
   
   //login function 
   int login(){
@@ -27,6 +40,57 @@
 	else{
 		
 	printf("Incorrect username or password :plz Try again/n");
+	
+}
+
+
+void deleteRecord() {
+	int caseID;
+	printf("Enter Case ID to Delete: ");
+	 
+	if (scanf("%d",&caseID) != 1) {
+		printf("Invalid Input! Case ID must be a number.\n");
+		while (getchar() != '\n');           //clear buffer
+		return;
+		
+	}
+	
+	FILE *fp = fopen(FILENAME, "rb");
+	if (!fp){
+		printf("Error opening file!\n");
+		return;
+	}
+	
+	FILE *temp = fopen("temp.dat", "wb");
+	if(!temp){
+		printf("Error opening temporary file!\n");
+		fclose(fp);
+		return;
+	}
+	
+	CrimeRecord cr;
+	int found = 0;
+	while (freaf(&cr, sizeof(CrimeRecord), 1, fp)) {
+		if (cr.caseID == caseID) {
+			found = 1;	
+		}
+		else{
+			fwrite(&cr, sizeof(CrimeRecord), 1, temp);
+		}
+	}
+	
+	fclose(fp);
+	fclose(temp);
+	
+	if (found) {
+		remove(FILENAME);
+		rename("temp.dat",FILENAME);
+		printf("Record with Case ID %d deleted successfully.\n", caseID);
+		
+	}
+	else{
+		printf("Record with Case ID %d not found.\n",caseID);
+	}
 	
 }
 
